b24d4ae29f63c76edec77aa63390540f791a593f
Aaron Shim <aaron.k.shim@gmail.com>
2018-06-19 21:50:57 -0400

    Exercise 4, completed in May.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN)
+  // if key pressed, then shade is black, else is white
+  @shade
+  M=-1
+  @KBD
+  D=M
+  @STARTLOOP
+  D; JGT
+  @shade
+  M=0
+
+(STARTLOOP)
+  // i = 0
+  @i
+  M=0
+
+(LOOP)
+  // if i >= 8191 goto MAIN
+  @i
+  D=M
+  @8191 
+  D=D-A
+  @MAIN
+  D; JGE
+ 
+  // SCREEN[i] = shade
+  // (There are only two registers, D and A/M, so we need to stash
+  //  the computed address to write to before using two registers to
+  //  read the preset shade value and then once that's safely stored in
+  //  D, bring back the precomputed address to read into A/M for write.) 
+  @i
+  D=M
+  @SCREEN
+  D=D+A
+  @nextblock
+  M=D
+  @shade
+  D=M
+  @nextblock
+  A=M 
+  M=D
+
+  // i = i + 1 
+  @i
+  M=M+1  
+
+  // goto LOOP
+  @LOOP
+  0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,80 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Pseudocode:
+//
+// times = if R1 < 0 then -R1 else R1
+// i = 0
+// accm = 0
+//
+// LOOP
+//   if i >= times goto STOP
+//   accm = accm + R0
+//   i = i + 1
+//   goto LOOP
+//
+// STOP
+//   R2 = if R1 > 0 then -accm else accm
+//
+// END
+//   goto END
+
+// times = if R1 < 0 then -R1 else R1
+@R1
+D=M
+@times
+M=D
+@STARTLOOP // Avoid flipping to positive if already positive.
+D; JGE
+@times
+M=-D
+
+(STARTLOOP)
+  // i = 0
+  @i
+  M=0
+
+  // accm = 0
+  @accm // Our intermediate result.
+  M=0
+
+(LOOP)
+  // if i >= times goto STOP
+  @i
+  D=M
+  @times
+  D=D-M
+  @STOP
+  D; JGE
+
+  // accm = accm + R0
+  @R0
+  D=M
+  @accm
+  M=M+D
+
+  // i = i + 1
+  @i
+  M=M+1
+
+  // goto LOOP
+  @LOOP
+  0; JMP
+
+(STOP)
+  // R2 = if R1 > 0 then -accm else accm
+  @accm
+  D=M
+  @R2
+  M=D  
+  @R1
+  D=M
+  @END
+  D; JGE
+  @R2
+  M=-M
+
+(END)
+  // goto END
+  @END
+  0; JMP
